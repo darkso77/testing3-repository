3d0d84069211421a6967d5bdefbb5d7e390fbecf
darkso77 <darkso77@wp.pl>
2022-07-26 14:38:52 +0200

    Added file Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test_ZamianaWyrazow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz wyrazy, np. \"a simple program to a computer\"");
+            var tekst = Console.ReadLine();
+            var result = ReverseStr.ReverseWord(tekst);
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+    }
+}
